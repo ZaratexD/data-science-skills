3e9c764d740dc5c8f072e2d3b6ed729087db6bcc
Angel Zarate <zarate@uw.edu>
2024-04-17 17:20:50 -0700

    Initialized work env, started gradient_descent.py

---
+++ Machine Learning/Gradient Descent/gradient_descent.py
@@ -0,0 +1,11 @@
+"""
+Finds optimized coefficients m and b in y = mx + b for line of best fit.
+Does so by finding minimum using gradient descent.
+Essential what sklearn.linear_model does 
+"""
+
+import numpy as np
+
+
+def gradient_descent(x, y):
+    pass
