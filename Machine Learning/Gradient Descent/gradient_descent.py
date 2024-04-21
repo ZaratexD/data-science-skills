dc361b01c268836fbf43fc7445085761cac53c1b
Angel Zarate <zarate@uw.edu>
2024-04-21 15:36:30 -0700

    descend cleaned up

--- Machine Learning/Gradient Descent/gradient_descent.py
+++ Machine Learning/Gradient Descent/gradient_descent.py
@@ -12,13 +12,15 @@ Repeats 3-5 until close to 0 or maximum # of steps
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def descend(x, y):
     curr_m = curr_b = 0
-    rate = 0.05
+    rate = 0.08
     n = x.shape[0]
     iterations = 500
+    cost_g = m = b = [0] * iterations
 
     for i in range(iterations):
         y_hat = curr_m * x + curr_b
@@ -26,11 +28,13 @@ def descend(x, y):
         slope_m = (-2 / n) * sum(x * (y - y_hat))
         slope_b = (-2 / n) * sum(y - y_hat)
 
+        cost_g[i] = cost
+        m[i] = curr_m
+        b[i] = curr_b
+
         # way for loop to reduce redundant code
         curr_m = curr_m - slope_m * rate
         curr_b = curr_b - slope_b * rate
-
-        print(f"m: {curr_b}, b: {curr_m}, cost {cost} iteration {i}")
 
 
 x = np.array([1, 2, 3, 4, 5])
